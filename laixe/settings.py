99025ac3d80dd93c6b4292c06db4b407a59b6eb8
MinhWin <quachquangminh2006@gmail.com>
2025-01-06 08:50:27 +0700

    add reporter app

--- laixe/settings.py
+++ laixe/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     "accounts",
     "home",
     "course",
+    "reporter",
 ]
 
 MIDDLEWARE = [
--- laixe/urls.py
+++ laixe/urls.py
@@ -21,10 +21,11 @@ from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
+    path("", include("home.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("", include("home.urls")),
     path("course/", include("course.urls")),
+    path("report/", include("reporter.urls")),
 ] + debug_toolbar_urls()
 
 if get_config("DEBUG"):
