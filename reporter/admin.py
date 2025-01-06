46455f7d79f8557a3879a1ec959736a126dff621
MinhWin <quachquangminh2006@gmail.com>
2025-01-06 13:19:15 +0700

    added report to admin and change verbose name

--- reporter/admin.py
+++ reporter/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Report, ReportCat
+
 # Register your models here.
+
+
+@admin.register(ReportCat)
+class ReportCatAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    pass
--- reporter/apps.py
+++ reporter/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig
 
 
 class ReporterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reporter'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "reporter"
+    verbose_name = "Báo cáo"
