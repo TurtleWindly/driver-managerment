d80e494a5bbbb6a50700d717170a9f071205de98
MinhWin <quachquangminh2006@gmail.com>
2025-01-07 08:26:35 +0700

    added unique constraint to LessonAttend

--- course/models/course_models.py
+++ course/models/course_models.py
@@ -13,11 +13,16 @@ class Course(models.Model):
         User, related_name="teaching_courses", blank=True, verbose_name="Giáo viên"
     )
     students = models.ManyToManyField(
-        DriverLicense, related_name="enrolled_courses", blank=True, verbose_name="Học viên"
+        DriverLicense,
+        related_name="enrolled_courses",
+        blank=True,
+        verbose_name="Học viên",
     )
     exam_date = models.DateField(blank=True, verbose_name="Ngày thi")
     graduation_date = models.DateField(blank=True, verbose_name="Ngày tốt nghiệp")
-    license_fk = models.ForeignKey(License, on_delete=models.SET_NULL, null=True, verbose_name="Loại bằng")
+    license_fk = models.ForeignKey(
+        License, on_delete=models.SET_NULL, null=True, verbose_name="Loại bằng"
+    )
     reports = GenericRelation(Report)
 
     def __str__(self):
@@ -46,16 +51,25 @@ class Lesson(models.Model):
 
 class LessonAttend(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name="Bài học")
-    student = models.ForeignKey(DriverLicense, on_delete=models.CASCADE, verbose_name="Học viên")
+    student = models.ForeignKey(
+        DriverLicense, on_delete=models.CASCADE, verbose_name="Học viên"
+    )
     present = models.BooleanField(default=False, verbose_name="Hiện diện")
 
     class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["lesson", "student"], name="unique_attend_lesson_student"
+            )
+        ]
         verbose_name = "Điểm danh lý thuyết"
         verbose_name_plural = "Điểm danh lý thuyết"
 
 
 class PracticeAttend(models.Model):
-    student = models.ForeignKey(DriverLicense, on_delete=models.CASCADE, verbose_name="Học viên")
+    student = models.ForeignKey(
+        DriverLicense, on_delete=models.CASCADE, verbose_name="Học viên"
+    )
     time = models.PositiveSmallIntegerField(default=1, verbose_name="Thời gian")
     date = models.DateField(auto_now_add=True, verbose_name="Ngày")
 
