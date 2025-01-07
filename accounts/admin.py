1029ca6c14cbc861ea589b626f744004418df193
MinhWin <quachquangminh2006@gmail.com>
2025-01-07 11:18:52 +0700

    Added DriverLicenseForm

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,10 +2,28 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
+from course.models import DriverLicense, Course
+
+from course.forms import DriverLicenseForm
+
 from .models import UserProfile
 
 # Register your models here.
 admin.site.unregister(User)
+
+
+class TeachingCourseInline(admin.TabularInline):
+    model = Course.teachers.through
+    extra = 1
+    verbose_name = "Lớp dạy"
+    verbose_name_plural = "Các lớp giảng dạy"
+
+
+class DriverLicenseInline(admin.StackedInline):
+    model = DriverLicense
+    form = DriverLicenseForm
+    extra = 1
+    show_change_link = True
 
 
 class UserProfileInline(admin.StackedInline):
@@ -17,4 +35,4 @@ class UserProfileInline(admin.StackedInline):
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    inlines = [UserProfileInline]
+    inlines = [UserProfileInline, TeachingCourseInline, DriverLicenseInline]
