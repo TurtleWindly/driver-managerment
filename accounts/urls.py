ed3956f89f3884387200cdf9c6d61088f2f452d4
MinhWin <quachquangminh2006@gmail.com>
2025-02-09 14:28:18 +0700

    student create account for sign up view

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
 	path("login/", views.LoginView.as_view(), name="login"),
 	path("logout/", LogoutView.as_view(), name="logout"),
+	path("signup/", views.student_create_account, name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth.views import LoginView
-from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render, HttpResponseRedirect
 from django.urls import reverse_lazy
 
-from .forms import CustomAuthenticationForm
+from course.models import DriverLicense
+
+from .models import UserProfile
+from .forms import CustomAuthenticationForm, StudentCreateAccountForm
 
 
 # Create your views here.
@@ -30,3 +34,29 @@ class LoginView(LoginView):
     def get_success_url(self) -> str:
         return reverse_lazy("home:home")
 
+
+def student_create_account(request, *args, **kwargs):
+    if request.method == "POST":
+        form = StudentCreateAccountForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create(
+                username=form.cleaned_data["cccd"],
+                password=form.cleaned_data["password"],
+                email=form.cleaned_data["email"],
+                last_name=form.cleaned_data["last_name"],
+                first_name=form.cleaned_data["first_name"],
+            )
+            UserProfile.objects.create(
+                user=new_user,
+                cccd=form.cleaned_data["cccd"],
+                dob=form.cleaned_data["dob"],
+                gender=form.cleaned_data["gender"],
+                phone=form.cleaned_data["phone"],
+            )
+            DriverLicense.objects.create(
+                user=new_user, license_fk=form.cleaned_data["license_fk"]
+            )
+            return HttpResponseRedirect(reverse_lazy("home:home"))
+    else:
+        form = StudentCreateAccountForm()
+    return render(request, "accounts/signup.html", {"form": form})
